d7e7ec4c9b5f9709c5dc25cb5416eabe145b0b47
mmangila <leroymangila5@gmail.com>
2021-11-01 12:35:50 +1000

    Put all variables in the function

--- analyse-rnaseq.R
+++ analyse-rnaseq.R
@@ -45,6 +45,53 @@ source("DESEQ2Code.R")
 source("edgeRCode.R")
 
 analyse.RNAseq <- function (project.folder,analysis, group) {
+
+    biocManagerLibs <- c("edgeR",
+                         "limma",
+                         "pcaMethods",
+                         "DESeq2",
+                         "biomaRt",
+                         "Rgraphviz",
+                         "topGO")
+    libs            <- c("seqinr",
+                         "car",
+                         "data.table",
+                         "ggalluvial",
+                         "ggplot2",
+                         "ggrepel",
+                         "ggthemes",
+                         "GO.db",
+                         "gplots",
+                         "grid",
+                         "lattice",
+                         "parallel",
+                         "pheatmap",
+                         "plyr",
+                         "RColorBrewer",
+                         "reshape2",
+                         "scales",
+                         "scatterplot3d",
+                         "statmod",
+                         "tidytext",
+                         "tidyverse",
+                         "viridis",
+                         "wesanderson",
+                         "devtools",
+                         "data.table")
+
+    lfc.suffixes <- data.frame(
+     Level = c(0,1.5,2),
+     Suffix = c("_detags.csv",
+                "_detags_1point5FC.csv",
+                "_detags_2FC.csv"
+     )
+    )
+
+    source("rewrittenHonoursCode.R")
+    source("CombinedCode.R")
+    source("DESEQ2Code.R")
+    source("edgeRCode.R")
+
     install_libraries(biocManagerLibs,BiocManager::install)
     install_libraries(libs,install.packages)
     devtools::install_github("INWTlab/rsync")
