6ce8c0f35ec17805d61981826c4b08dd1cadc045
mmangila <65453301+mmangila@users.noreply.github.com>
2021-11-02 13:11:21 +1000

    Update edgeRCode.R

--- edgeRCode.R
+++ edgeRCode.R
@@ -14,7 +14,7 @@ find_de_edger <- function (old.dge, group, keyfile, paths, analysis) {
   de_edger_pca(v, group, keyfile, paths)
   print("Begin DE gene table generation")
   gene.names <- as.character(rownames(dge$counts))
-  de_edger_tables(keyfile, group, fit, paths, design, gene.names, analysis)
+  de_edger_tables(keyfile, group, fit, paths, design, gene.names, analysis, v)
 }
 
 de_edger_mds <- function (dge, group, keyfile, paths, v) {
@@ -120,7 +120,7 @@ de_edger_pca <- function (v, group, keyfile, paths) {
   dev.off()
 }
 
-de_edger_tables <- function (keyfile, group, fit, paths, design, gene.names, analysis) {
+de_edger_tables <- function (keyfile, group, fit, paths, design, gene.names, analysis, v) {
   combos <- eval(
     parse(
       text = paste0(
