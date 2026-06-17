29fcc47ccee99f3a7d6ea75071570038bfa93aa9
mmangila <65453301+mmangila@users.noreply.github.com>
2026-06-17 14:06:59 +1000

    Update topGO_functions.R

--- topGO_functions.R
+++ topGO_functions.R
@@ -97,7 +97,7 @@ analyse_topgo <- function(ontology_to_test,
     de_go_genes <- go_genes[[1]][go_genes[[1]] %in% de_locus]
     print(paste0("Getting genes for ", go_term))
 
-    if (funcs != NULL) {
+    if (funcs[, 1] > 1) {
       go_table <- funcs[funcs[, 1] %in% de_go_genes, ]
     } else {
       go_table <- de_go_genes
