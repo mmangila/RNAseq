102781b026c9fbdef29762ef9a5244fcb876159e
mmangila <65453301+mmangila@users.noreply.github.com>
2026-08-16 03:02:46 +1000

    Update topGO_functions.R

--- topGO_functions.R
+++ topGO_functions.R
@@ -26,7 +26,8 @@ analyse_topgo <- function(ontology_to_test,
                    paste(test_name, de_direction, "GO", sep = "_"), sep = "/"),
              showWarnings = FALSE, recursive = TRUE)
   
-  alltag_table <- read_csv(alltag_table_file)
+  alltags_table <- read_csv(alltag_table_file)
+  de_table      <- alltags_table[which(alltags_table$padj < 0.05), ]
   gene_universe <- alltags_table %>% pull(func_focus)
 
   if (de_direction == "up") {
@@ -75,13 +76,14 @@ analyse_topgo <- function(ontology_to_test,
                                 orderBy = "classicFisher"))
 
   all_res_fdr   <- all_res %>%
-    mutate(fdr = p.adjust(Fisher_p_value, method = "BH"),
+    mutate(fdr    = p.adjust(Fisher_p_value,
+                             method = "BH"),
            Aspect = ontology_to_test)
 
   sig_res_fdr   <- all_res_fdr %>% filter(fdr < go_pval)
 
   print(paste("Significantly enriched",
-               ontology_to_test,
+              ontology_to_test,
               "GO terms in",
               paste0(de_direction, "regulated genes in"),
               test_name,
@@ -115,32 +117,27 @@ analyse_topgo <- function(ontology_to_test,
 
   #### Make graphs
   print("Making GO graphs")
-  result_ks      <- runTest(go_data, algorithm = "classic", statistic = "ks")
-  result_ks_elim <- runTest(go_data, algorithm = "elim",    statistic = "ks")
+  result_ks <- runTest(go_data, algorithm = "elim",    statistic = "ks")
 
-  node_path <- paste0(out_dir,
-                      "/",
-                      test_name,
-                      "_",
-                      de_direction,
-                      "_",
-                      ontology_to_test,
-                      "_nodegraphs.pdf")
+  node_path <- paste(out_dir,
+                     paste(test_name,
+                           de_direction,
+                           ontology_to_test,
+                           "nodegraphs.pdf", sep = "_"), sep = "/")
 
   pdf(node_path)
   par(cex = 0.5)
   try(showSigOfNodes(go_data,
-                 score(result_ks_elim),
-                 firstSigNodes = 5,
-                 useInfo       = "all"))
+                     score(result_ks),
+                     firstSigNodes = 5,
+                     useInfo       = "all"))
   try(printGraph(go_data,
-             result_fisher,
-             firstSigNodes = 5,
-             fn.prefix     = "tGO",
-             useInfo       = "all",
-             pdfSW         = TRUE))
+                 result_fisher,
+                 firstSigNodes = 5,
+                 fn.prefix     = "tGO",
+                 useInfo       = "all",
+                 pdfSW         = TRUE))
   dev.off()
-
 }
 
 
@@ -159,11 +156,11 @@ go_plot_comparison <- function(ontology_to_test,
                            ontology_to_test,
                            sep = "_")
 
-  big_data_selection <- big_data %>%
-    filter(sample %in% plot_comparison) %>%
+  big_data_selection <- big_data                        %>%
+    filter(sample %in% plot_comparison)                 %>%
     mutate(enrichment_ratio = Significant/Expected,
-           GO_term          = paste(GO.ID, Term)) %>%
-    arrange(enrichment_ratio) %>%
+           GO_term          = paste(GO.ID, Term))       %>%
+    arrange(enrichment_ratio)                           %>%
     mutate(GO_term = factor(GO_term, levels = GO_term))
 
   g <- ggplot(big_data_selection,
@@ -171,19 +168,15 @@ go_plot_comparison <- function(ontology_to_test,
                   x = enrichment_ratio,
                   size = Significant,colour = fdr)) +
         geom_point()                                +
-    theme_minimal() +
-    scale_colour_viridis(
-      option = "A",
-      direction = -1,
-      begin = 0.2,
-      end = 0.8
-    ) +
-    labs(
-      size = "Number of genes",
-      colour='FDR',
-      y = NULL,
-      x = "Relative enrichment (Observed/Expected)"
-    )
+        theme_minimal()                             +
+    scale_colour_viridis(option    = "A",
+                         direction = -1,
+                         begin     = 0.2,
+                         end       = 0.8)           +
+    labs(size   = "Number of genes",
+         colour ='FDR',
+         y      = NULL,
+         x      = "Relative enrichment (Observed/Expected)")
 
   pdf(paste0(data_folder, "/", plot_comparison, "_GO_plot.pdf"))
   print(g)
@@ -192,19 +185,18 @@ go_plot_comparison <- function(ontology_to_test,
 
 analyse_go <- function (funcs, func_focus, project_folder, combos, project_paths, de_logfc, go_pval) {
 
-  geneDescription_GO <- funcs %>%
+  geneDescription_GO <- funcs     %>%
     dplyr::select(func_focus, GO) %>%
     dplyr::rename(DB_Object_ID = func_focus, GOid_list = GO)
   geneDescription_GO <- geneDescription_GO %>% filter(!is.na(GOid_list))
 
   write_tsv(geneDescription_GO,
             paste0(project_folder, "/", project_folder, "_readMappings.tsv"),
-            col_names = F)
+            col_names = FALSE)
 
-  geneid_to_go <- readMappings(file = paste0(project_folder,
-                                          "/",
-                                          project_folder,
-                                          "_readMappings.tsv"))
+  geneid_to_go <- readMappings(file = paste(project_folder,
+                                            paste0(project_folder,
+                                                   "_readMappings.tsv"))
 
   go_opts <- expand.grid(c("BP", "MF", "CC"), c("up", "down"))
 
@@ -213,7 +205,7 @@ analyse_go <- function (funcs, func_focus, project_folder, combos, project_paths
       sapply(seq_along(go_opts[, 1]), function (y) {
         analyse_topgo(as.character(go_opts[y, 1]),
                       as.character(go_opts[y, 2]),
-                      paste0(combos[1,x], ".vs.", combos[2,x]),
+                      paste0(combos[1, x], ".vs.", combos[2, x]),
                       paste(project_paths[3],
                             "Combined",
                             de_combo,
@@ -222,7 +214,8 @@ analyse_go <- function (funcs, func_focus, project_folder, combos, project_paths
                             paste0(combos[1,x], ".vs.", combos[2,x]),
                             "_alltags.csv",
                             sep = "/"),
-                      paste0(project_paths[3], "/Combined/GO_tests/"),
+                      paste(project_paths[3], "Combined", de_combo, "GO_tests",
+                            sep = "/"),
                       funcs,
                       func_focus,
                       geneid_to_go,
@@ -244,21 +237,18 @@ analyse_go <- function (funcs, func_focus, project_folder, combos, project_paths
   Sample_names <- file_names %>% str_replace(".csv", "")
 
   data <- data_frame(sample = Sample_names, paths = sample_paths) %>%
-    mutate(
-      file_contents = map(sample_paths, ~ read_csv(., col_names = TRUE))
-    ) %>%
+    mutate(file_contents = map(sample_paths,
+                               ~ read_csv(., col_names = TRUE)))  %>%
     dplyr::select(-paths)
 
-  big_data <- unnest(data[which(!isEmpty(data$file_contents)),],
+  big_data <- unnest(data[which(!isEmpty(data$file_contents)), ],
                      cols = c(file_contents))
 
-  sapply(1:combos[1, ], function (x) {
-    map2(
-      rep(c("BP", "MF", "CC"), 2), rep(c("up", "down"), each = 3),
-      go_plot_comparison,
-      test_name  = paste0(combos[1,x], ".vs.", combos[2,x]),
-      big_data   = big_data,
-      data_folder = data_folder
-    )
+  sapply(seq_along(combos[1, ]), function (x) {
+    map2(rep(c("BP", "MF", "CC"), 2), rep(c("up", "down"), each = 3),
+         go_plot_comparison,
+         test_name  = paste0(combos[1,x], ".vs.", combos[2,x]),
+         big_data   = big_data,
+         data_folder = data_folder)
   })
 }
