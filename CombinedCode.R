d470aea7fb2edf223b809d820b9378329382c0e5
mmangila <65453301+mmangila@users.noreply.github.com>
2026-08-20 23:42:56 +1000

    Update CombinedCode.R

--- CombinedCode.R
+++ CombinedCode.R
@@ -121,7 +121,7 @@ find_combined_de <-  function(keyfile,
 
   combos <- combn(as.data.frame(keyfile %>% distinct(sample_group))[,1], 2)
 
-  sink(file = paste0(combined_folder, "/de_genes_summary.txt"))
+  sink(file = paste0(combined_folder, de_combo, "de_genes_summary.txt", sep = "/"))
 
   sapply(seq_along(combos[1, ]), function (comparison) {
     combine_desets(combos,
